0fb62859828a73ac185b9dda4a02deaf93b36451
Frieder Pankratz <pankratz@in.tum.de>
2012-08-16 15:19:03 +0200

    remove generated file

--- src/utVisualization/WebGui/config.h
+++
@@ -1,5 +0,0 @@
-#ifndef __UBITRACK_CONFIG_H_INCLUDED__
-#define __UBITRACK_CONFIG_H_INCLUDED__
-#define UBITRACK_COMPONENTS_PATH "D:\\Projekte\\UbiTrackTest\\bin\\ubitrack"
-#define UBITRACK_COMPONENTS_RELAVIVEPATH "ubitrack"
-#endif
