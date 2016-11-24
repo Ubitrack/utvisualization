96cfe3bc3041bf4b4c22d32953ed27b059f04cd2
Ulrich Eck <ueck@net-labs.de>
2016-11-24 09:22:58 +0100

    remove GLUT support start cleaning up

--- src/utVisualization/Render/RenderGLUT/GL/freeglut.h
+++
@@ -1,8 +0,0 @@
-#ifdef __APPLE__
-	#include <GL/freeglut.h>
-	//#include <GLUT/glut.h>
-	#define glutSetOption(a,b)
-	#define glutMainLoopEvent glutCheckLoop
-#else
-	#include <GL/freeglut.h>
-#endif
