af65c1a48f953e0298b85eafb91b00e7fa191d61
Ulrich Eck <ueck@net-labs.de>
2018-01-14 16:38:33 +0100

    initial draft of conan build

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 utvisualization
 ===============
+This is the utvisualization Ubitrack submodule.
+
+Description
+----------
+The utvisualization contains an interface to visualization modules. It also contains the utGLFWConsole.
+
+## For Users: Use this package
+
+### Basic setup
+
+    $ conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan True
+    $ conan install ubitrack_visualization/1.3.0@ubitrack/stable
+
+### Project setup
+
+If you handle multiple dependencies in your project is better to add a *conanfile.txt*
+
+    [requires]
+    ubitrack_visualization/1.3.0@ubitrack/stable
+
+    [generators]
+    cmake
+    txt
+
+Complete the installation of requirements for your project running:
+
+    $ mkdir build && cd build && conan install ..
+    
+Note: It is recommended that you run conan install from a build directory and not the root of the project directory.  This is because conan generates *conanbuildinfo* files specific to a single build configuration which by default comes from an autodetected default profile located in ~/.conan/profiles/default .  If you pass different build configuration options to conan install, it will generate different *conanbuildinfo* files.  Thus, they shoudl not be added to the root of the project, nor committed to git. 
+
+## For Packagers: Publish this Package
+
+The example below shows the commands used to publish to ulricheck conan repository. To publish to your own conan respository (for example, after forking this git repository), you will need to change the commands below accordingly. 
+
+## Build and package 
+
+The following command both runs all the steps of the conan file, and publishes the package to the local system cache.  This includes downloading dependencies from "build_requires" and "requires" , and then running the build() method. 
+
+    $ conan create . ubitrack/stable
+    
+## Add Remote
+
+    $ conan remote add camp "https://conan.campar.in.tum.de" True
+
+## Upload
+
+    $ conan upload -r camp ubitrack_visualization/1.3.0@ubitrack/stable
---
+++ conanfile.py
@@ -0,0 +1,75 @@
+from conans import ConanFile, CMake
+
+
+class UbitrackCoreConan(ConanFile):
+    name = "ubitrack_visualization"
+    version = "1.3.0"
+
+    description = "Ubitrack Visualization Library and Components"
+    url = "https://github.com/Ubitrack/utvisualization.git"
+    license = "GPL"
+
+    short_paths = True
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    options = {"shared": [True, False],
+               "enable_glfwconsole": [True, False]}
+    requires = (
+        "ubitrack_core/%s@ubitrack/stable" % version,
+        "ubitrack_vision/%s@ubitrack/stable" % version,
+        "ubitrack_dataflow/%s@ubitrack/stable" % version,
+       )
+
+    default_options = (
+        "shared=True",
+        "enable_glfwconsole=True",
+        )
+
+    # all sources are deployed with the package
+    exports_sources = "apps/*", "cmake/*", "components/*", "doc/*", "src/*", "CMakeLists.txt"
+
+    def build_requirements(self):
+        if self.options.enable_glfwconsole:
+            self.build_requires("glfw/3.2.1@camposs/stable")
+
+    def configure(self):
+        if self.options.shared:
+            self.options['ubitrack_core'].shared = True
+            self.options['ubitrack_vision'].shared = True
+            self.options['ubitrack_dataflow'].shared = True
+
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin") # From bin to bin
+        self.copy(pattern="*.dylib*", dst="lib", src="lib") 
+       
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions['BUILD_SHARED_LIBS'] = self.options.shared
+        cmake.definitions['ENABLE_GLFWCONSOLE'] = self.options.enable_glfwconsole
+        cmake.configure()
+        cmake.build()
+        cmake.install()
+
+    def package(self):
+        # self.copy("*.h", dst="include", src="src")
+        # self.copy("*.lib", dst="lib", excludes="*/lib/ubitrack/*.lib", keep_path=False)
+        # self.copy("*.dll", dst="bin", excludes="*/lib/ubitrack/*.dll", keep_path=False)
+        # self.copy("*.dylib*", dst="lib", excludes="*/lib/ubitrack/*.dylib", keep_path=False)
+        # self.copy("*.so", dst="lib", excludes="*/lib/ubitrack/*.so", keep_path=False)
+        # self.copy("*.a", dst="lib", keep_path=False)
+        # self.copy("*", dst="bin", src="bin", keep_path=False)
+
+        # # components
+        # self.copy("ubitrack/*.lib", dst="lib/ubitrack", keep_path=False)
+        # self.copy("ubitrack/*.dll", dst="bin/ubitrack", keep_path=False)
+        # self.copy("ubitrack/*.dylib*", dst="lib/ubitrack", keep_path=False)
+        # self.copy("ubitrack/*.so", dst="lib/ubitrack", keep_path=False)
+        pass
+
+    def package_info(self):
+        suffix = ""
+        if self.settings.os == "Windows":
+            suffix += self.version.replace(".", "")
+            if self.settings.build_type == "Debug":
+                suffix += "d"
+        self.cpp_info.libs.append("utfacade%s" % (suffix))
